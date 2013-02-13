f1a1d0f1d81a7bd3d1eb2a113c65f67f5bff76a0
Maxim Kremenev <ezotrank@gmail.com>
2013-02-13 14:04:57 +0300

    #TOON-54 fix

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,5 +2,6 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
     @seo_tags = { title: @movie.title, description: @movie.description, image: @movie.default_image }
+    @head_section = 'portfolio'
   end
 end
