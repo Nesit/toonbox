64e405a95afe50489bb84ba8c7b7676c60406329
Maxim Kremenev <ezotrank@gmail.com>
2013-02-14 16:22:27 +0300

    #TOON-60 fix

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -1,6 +1,6 @@
 class PersonsController < ApplicationController
   def index
-    @staffs = Staff.all
+    @staff = params[:id] ? Staff.find(params[:id]) : Staff.first
     @seo_tags = VirtualPage.find_by_name('persons').seo_tags
     @head_section = 'about_studio'
     @submenu_section = 'persons'
