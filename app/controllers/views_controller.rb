6bff9af23b50423e623071f9b355c966a9dd2d74
Artjom Juferov <derevjaginti@gmail.com>
2014-04-09 09:05:55 +0300

    add view count to checkboxes

--- app/controllers/views_controller.rb
+++ app/controllers/views_controller.rb
@@ -2,10 +2,12 @@ class ViewsController < ApplicationController
   def refresh
     p params[:episodes]
     episodes = params[:episodes]
-    episodes.each do |ep_id|
-      ep = MovieEpisode.find(ep_id)
-      ep.update_youtube_view_count!
-      ep.update_vimeo_view_count!
+    if episodes != nil 
+      episodes.each do |ep_id|
+        ep = MovieEpisode.find(ep_id)
+        ep.update_youtube_view_count!
+        ep.update_vimeo_view_count!
+      end
     end
     # %x[rake movies:update_youtube_view_count]
     # %x[rake movies:update_vimeo_view_count]
