0917db3dd2c2345be212af919465882d0f6c5386
Artjom Juferov <derevjaginti@gmail.com>
2014-04-09 07:18:56 +0300

    mistake

--- db/migrate/20140409041701_add_column_to_table_episodes.rb
+++ db/migrate/20140409041701_add_column_to_table_episodes.rb
@@ -1,6 +1,5 @@
 class AddColumnToTableEpisodes < ActiveRecord::Migration
   def change
-    add_column :table_episodes, :add_video_url, :string
-    add_column :table_episodes, :string, :string
+    add_column :movie_episodes, :add_video_url, :string
   end
 end
